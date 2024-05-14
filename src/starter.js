39bc6010799ed23485100475ce70281c6beaa19f
Maurice-Jah <mauricejahwizwid@gmail.com>
2024-05-14 14:58:31 +0100

    Fetch data from the API

--- src/starter.js
+++ src/starter.js
@@ -16,42 +16,8 @@ function getWeatherIcon(wmoCode) {
   return icons.get(arr);
 }
 
-function convertToFlag(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
-
 function formatDay(dateStr) {
   return new Intl.DateTimeFormat("en", {
     weekday: "short",
   }).format(new Date(dateStr));
-}
-
-async function getWeather(location) {
-  try {
-    // 1) Getting location (geocoding)
-    const geoRes = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-    );
-    const geoData = await geoRes.json();
-    console.log(geoData);
-
-    if (!geoData.results) throw new Error("Location not found");
-
-    const { latitude, longitude, timezone, name, country_code } =
-      geoData.results.at(0);
-    console.log(`${name} ${convertToFlag(country_code)}`);
-
-    // 2) Getting actual weather
-    const weatherRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-    );
-    const weatherData = await weatherRes.json();
-    console.log(weatherData.daily);
-  } catch (err) {
-    console.err(err);
-  }
 }
