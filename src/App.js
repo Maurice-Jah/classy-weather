ffd4e66018f3a1a5a445f9a521c5ee5ec401d731
Maurice-Jah <mauricejahwizwid@gmail.com>
2024-05-15 03:17:55 +0100

    Delete starter.js and format weather display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
+}
+
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    weekday: "short",
+  }).format(new Date(dateStr));
+}
+
 function convertToFlag(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
@@ -110,6 +134,7 @@ class Weather extends React.Component {
               min={min.at(i)}
               code={codes.at(i)}
               key={date}
+              isToday={i === 0}
             />
           ))}
         </ul>
@@ -120,11 +145,12 @@ class Weather extends React.Component {
 
 class Day extends React.Component {
   render() {
-    const { date, max, min, code } = this.props;
+    const { date, max, min, code, isToday } = this.props;
+
     return (
       <li className="day">
-        <span>{code}</span>
-        <p>{date}</p>
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? "Today" : formatDay(date)}</p>
         <p>
           {Math.floor(min)}&deg; &mdash; {Math.ceil(max)}&deg;
         </p>
